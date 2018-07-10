d3e7c71fd7b260ac20b354a29a19a0528f35c1e6
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-10 23:41:34 +0930

    root route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'login/rootview'
+  #root routes
+  root 'login#rootview'
+  #get 'login/rootview'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
