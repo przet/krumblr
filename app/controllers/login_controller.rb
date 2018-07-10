ba1de7799aa2ef57f1f00482113a8a51a63c1292
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-11 08:52:30 +0930

    Some initial code for the login controller.
    Based on 
    https://www.sitepoint.com/rails-userpassword-authentication-from-scratch-part-i/
    The commented lines give ruby syntax error though, have to find out what 
    is going on there.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,16 @@
 class LoginController < ApplicationController
   def rootview
+    @user = User.new
   end
+  def create
+    @user = User.new(params[:user])
+    #if @user.save
+    #  flash[:notice] = "Successful Sign up!"
+    #  flash[:color] = "valid"
+    #else
+    #  flash [:notice] = "Something has gone wrong"
+    #  flash [:color] = "invalid"
+    #end
+render "rootview"
+end
 end
