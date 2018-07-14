1d3ae98c7f43c94dac1326f292cd6a30b4a849eb
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-14 18:27:57 +0930

    Routes and resources for new controller 'Blogs' (not 'Blog')

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   get 'posts/show'
   get 'posts/index'
   get 'posts/new'
+  get 'blogs/show'
+  get 'blogs/index'
+  get 'blogs/new'
   devise_for :users
   resources :posts
+  resources :blogs
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
