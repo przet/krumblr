3f382354781a5990418ec0939cb1fb41ef2d073b
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-14 17:41:49 +0930

    Migrating new 'blog' model title row

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_072247) do
+ActiveRecord::Schema.define(version: 2018_07_14_080935) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
