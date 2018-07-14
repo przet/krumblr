56f597871dfa99fe437d5210d4187661f43412b0
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-14 15:04:53 +0930

    Blog body rendering!
    Got it! Hunting around, trying things out, and then say that :content 
    was in the post controller rather than :body. And these are the field in 
    our model that we created (there is no content. So that is why 
    :description etc is not working with simple form, as it does not exist!)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :body)
   end
 
 end
