38d28afcddac79d100e577d89ec2b06bfbc98509
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-15 18:41:24 +0930

    Created a copy of Post routes nested in blog resources, and also a non-nested posts resource.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :blogs do
     resources :posts
+    get 'posts/show'
+    get 'posts/index'
+    get 'posts/new'
   end
+  resources :posts
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
