1077641a38489c302f3d375419bea2294ecc6962
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-15 12:01:30 +0930

    Migrate new comment model rows (name and comment) into db table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_072247) do
+ActiveRecord::Schema.define(version: 2018_07_15_022947) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "blog_id"
   end
 
   create_table "users", force: :cascade do |t|
