bbedba258fb7ae3b32d97eef29f64cda0e79a89c
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-15 12:07:16 +0930

    Adding a post ID to comments.
    Following 
    https://web-crunch.com/lets-build-with-ruby-on-rails-blog-with-comments/.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_15_022947) do
+ActiveRecord::Schema.define(version: 2018_07_15_023500) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_07_15_022947) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
