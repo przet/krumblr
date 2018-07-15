deb345e17e6852db9cf01baa54ec44d2df503a5f
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-15 13:16:21 +0930

    Nested resource route for comments in post.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/new'
   devise_for :users
-  resources :posts
-  resources :comments
+  resources :posts do
+    resources :comments
+  end
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
