75e16eea9b49659a6f8a2b4189a21d3e7d2a9608
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-15 11:59:04 +0930

    Added comments resource to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'posts/new'
   devise_for :users
   resources :posts
+  resources :comments
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
