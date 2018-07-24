90ead838a4b8ee6a83cfde78725c66ff755b4541
Philippe Rzetelski (eeepc-linux) <p.rzetelski.note@gmail.com>
2018-07-24 15:04:32 +0930

    Commenting out routes.
    These seem to be redundant. Leaving them commented in for now, until I
    am sure they are not needed (I don't think they are) and then I will
    remove them

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/show'
-  get 'posts/index'
-  get 'posts/new'
-  get 'blogs/show'
-  get 'blogs/index'
-  get 'blogs/new'
+  #get 'posts/show'
+  #get 'posts/index'
+  #get 'posts/new'
+  #get 'blogs/show'
+  #get 'blogs/index'
+  #get 'blogs/new'
   devise_for :users
   resources :posts do
     resources :comments
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     get 'posts/index'
     get 'posts/new'
   end
-  resources :posts
+  #resources :posts
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
