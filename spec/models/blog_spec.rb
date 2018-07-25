e252f5604ff8eb40d06ab3748c7148de70acc3b6
Philippe Rzetelski (eeepc-linux) <p.rzetelski.note@gmail.com>
2018-07-25 20:39:42 +0930

    More test cases - but some fail, need to write to database, and persist.
    I know about let and let!, and just trying to figure out where to put
    them in (putting them after the first (zero) blog test yields a failure
    of that tests, so it seems like the write to db is done first)

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+
   it "has 0 blogs at the start" do
       expect(Blog.count).to eq(0)
   end
+
+  it "has 1 blog after creating" do
+      Blog.create
+      expect(Blog.count).to eq(1)
+  end
+
+  it "has 1 blog persisting after creating" do
+      expect(Blog.count).to eq(1)
+  end
+
+  
 end
