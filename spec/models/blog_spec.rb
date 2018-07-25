ff4153a2ac404042b557e98299935e873515cbfb
Philippe Rzetelski (eeepc-linux) <p.rzetelski.note@gmail.com>
2018-07-25 20:01:52 +0930

    First spec up and running! Blog model, testing that 0 blogs at start, returns zero blogs.
    Running rake rspec results in a pass (amonsgst the many other
    "non-implemented" specs. Also, setting the expected to 1 throws a
    failure (as it should) - that was me testing the testing code!

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "has 0 blogs at the start" do
+      expect(Blog.count).to eq(0)
+  end
 end
