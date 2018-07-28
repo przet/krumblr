e685e4b523c3c180450b7f015adea8684d09ad55
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-28 13:51:43 +0930

    Removed old routes that are not needed any longer

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  #get 'posts/show'
-  #get 'posts/index'
-  #get 'posts/new'
-  #get 'blogs/show'
-  #get 'blogs/index'
-  #get 'blogs/new'
   devise_for :users
   resources :posts do
     resources :comments
@@ -15,7 +9,6 @@ Rails.application.routes.draw do
     get 'posts/index'
     get 'posts/new'
   end
-  #resources :posts
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
