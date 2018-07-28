ae328471d93fd3bf51c1eae331860558095c9570
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-28 18:13:33 +0930

    Removed redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   end
   resources :blogs do
     resources :posts
-    get 'posts/show'
-    get 'posts/index'
-    get 'posts/new'
   end
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
