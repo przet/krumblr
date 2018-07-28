7e7c3e0a6c2adf7dffeb94d7cd64fd6b52fef8bd
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-28 18:07:49 +0930

    Removed hash for "Create a new blog" and repaced with path helper; removed :method => :get on sign in and sign out; fixed indentation on blog controller spec file.
    I still have a hash for blog index, however the path helpers did not
    work here.

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe BlogsController, type: :controller do
   describe "get #index" do
     it "renders the :index view" do
         get :index
-            response.should render_template :index
+          response.should render_template :index
     end
   end
 end
