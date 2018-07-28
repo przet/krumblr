e5cfbb78841581622f3fcf027f764ffa5c55e587
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-07-28 13:09:59 +0930

    A commented out file created in an attempt to rollback the active record entries.
    This was taken from a stack overflow answer, however I have no use of it
    at the moment.

---
+++ spec/support/shared_db_connection.rb
@@ -0,0 +1,10 @@
+### Leave commented for now ###
+#class ActiveRecord::Base
+#  mattr_accessor :shared_connection
+#  @@shared_connection = nil
+
+#  def self.connection
+#    @@shared_connection || retrieve_connection
+#  end
+#end
+#ActiveRecord::Base.shared_connection=ActiveRecord::Base.connection
